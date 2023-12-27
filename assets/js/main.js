b85c3709aaddf4792a6c2af9910a5d88d13f347f
abothke <andrebothke@gmail.com>
2023-12-27 11:27:40 +0100

    Abfrage für Geschlecht in Ternary operator und Formatierung

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,25 @@
 // Ein Projekt zur Berechnung des Tagesbedarfs an Kalorien nach der Harris-Benedict-Formel nach Geschlecht inkl. Beachtung des PAL-Wertes (Physical Activity Level)
-
-// Variablen
-let geschlecht = document.querySelector("#male").checked;
-let gewicht = 0;
-let groesse = 0;
-let alter = 0;
-let pal = 0;
-let grundumsatz = 0;
-let gesamtumsatz = 0;
-let ergebnis = 0;
-
-// Funktionen
-
-// Funktion zur Berechnung des Grundumsatzes und des Gesamtumsatzes nach Geschlecht und PAL-Wert (Physical Activity Level) und Ausgabe des Ergebnisses in #resultValue und #resultValuePal
 // nach der Harris-Benedict-Formel (https://de.wikipedia.org/wiki/Harris-Benedict-Formel)
+// Funktion zur Berechnung des Grundumsatzes und des Gesamtumsatzes nach Geschlecht und PAL-Wert (Physical Activity Level) und Ausgabe des Ergebnisses in #resultValue und #resultValuePal
 
 const calcTdee = (event) => {
     event.preventDefault(); // Verhindert das Neuladen der Seite nach Klick auf #calcButton
-    weight = document.querySelector("#weightVal").value; // Wert aus #weightVal wird in Variable weight gespeichert
-    height = document.querySelector("#heightVal").value; // Wert aus #heightVal wird in Variable height gespeichert
-    age = document.querySelector("#ageVal").value; // Wert aus #ageVal wird in Variable age gespeichert
-    pal = document.querySelector("#palLevelSelect").value; // Wert aus #palLevelSelect wird in Variable pal gespeichert
-    if (geschlecht === true) {
-        // Wenn #male checked ist, dann wird der Grundumsatz nach der Harris-Benedict-Formel für Männer berechnet
-        grundumsatz = 66.47 + 13.7 * weight + 5 * height - 6.8 * age;
-    } else {
-        // Wenn #male nicht checked ist, dann wird der Grundumsatz nach der Harris-Benedict-Formel für Frauen berechnet
-        grundumsatz = 655.1 + 9.6 * weight + 1.8 * height - 4.7 * age;
-    }
-    gesamtumsatz = grundumsatz * pal; // Der Gesamtumsatz wird berechnet
+
+    let weight = document.querySelector("#weightVal").value; // Wert aus #weightVal wird in Variable weight gespeichert
+    let height = document.querySelector("#heightVal").value; // Wert aus #heightVal wird in Variable height gespeichert
+    let age = document.querySelector("#ageVal").value; // Wert aus #ageVal wird in Variable age gespeichert
+    let pal = document.querySelector("#palLevelSelect").value; // Wert aus #palLevelSelect wird in Variable pal gespeichert
+    let geschlecht = document.querySelector("#male").checked; // Wert aus #male wird in Variable geschlecht gespeichert
+
+    // Der Grundumsatz wird berechnet wenn geschlecht == true (männlich), dann wird die erste Formel (für Männer) verwendet, wenn geschlecht == false (weiblich), dann wird die zweite Formel (für Frauen) verwendet
+    geschlecht == true ? (grundumsatz = 66.47 + 13.7 * weight + 5 * height - 6.8 * age) : (grundumsatz = 655.1 + 9.6 * weight + 1.8 * height - 4.7 * age);
+
+    let gesamtumsatz = grundumsatz * pal; // Der Gesamtumsatz wird berechnet
+
     document.querySelector("#resultValue").textContent = ` ${grundumsatz.toFixed(0)} kcal`; // Der Grundumsatz wird in #resultValue ausgegeben
     document.querySelector("#resultValuePal").textContent = ` ${gesamtumsatz.toFixed(0)} kcal`; // Der Gesamtumsatz wird in #resultValuePal ausgegeben
+
+    console.log(geschlecht);
 };
 
 // Eventlistener für #calcButton
