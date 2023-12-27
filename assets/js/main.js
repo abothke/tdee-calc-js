4eaa32e4cdc61a4922969a03ebeae92d1c559890
abothke <andrebothke@gmail.com>
2023-12-27 11:10:22 +0100

    kleinere Formatierungen

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,25 +16,21 @@ let ergebnis = 0;
 // nach der Harris-Benedict-Formel (https://de.wikipedia.org/wiki/Harris-Benedict-Formel)
 
 const calcTdee = (event) => {
-  event.preventDefault(); // Verhindert das Neuladen der Seite nach Klick auf #calcButton
-  weight = document.querySelector("#weightVal").value; // Wert aus #weightVal wird in Variable weight gespeichert
-  height = document.querySelector("#heightVal").value; // Wert aus #heightVal wird in Variable height gespeichert
-  age = document.querySelector("#ageVal").value; // Wert aus #ageVal wird in Variable age gespeichert
-  pal = document.querySelector("#palLevelSelect").value; // Wert aus #palLevelSelect wird in Variable pal gespeichert
-  if (geschlecht === true) {
-    // Wenn #male checked ist, dann wird der Grundumsatz nach der Harris-Benedict-Formel für Männer berechnet
-    grundumsatz = 66.47 + 13.7 * weight + 5 * height - 6.8 * age;
-  } else {
-    // Wenn #male nicht checked ist, dann wird der Grundumsatz nach der Harris-Benedict-Formel für Frauen berechnet
-    grundumsatz = 655.1 + 9.6 * weight + 1.8 * height - 4.7 * age;
-  }
-  gesamtumsatz = grundumsatz * pal; // Der Gesamtumsatz wird berechnet
-  document.querySelector("#resultValue").textContent = ` ${grundumsatz.toFixed(
-    0
-  )} kcal`; // Der Grundumsatz wird in #resultValue ausgegeben
-  document.querySelector(
-    "#resultValuePal"
-  ).textContent = ` ${gesamtumsatz.toFixed(0)} kcal`; // Der Gesamtumsatz wird in #resultValuePal ausgegeben
+    event.preventDefault(); // Verhindert das Neuladen der Seite nach Klick auf #calcButton
+    weight = document.querySelector("#weightVal").value; // Wert aus #weightVal wird in Variable weight gespeichert
+    height = document.querySelector("#heightVal").value; // Wert aus #heightVal wird in Variable height gespeichert
+    age = document.querySelector("#ageVal").value; // Wert aus #ageVal wird in Variable age gespeichert
+    pal = document.querySelector("#palLevelSelect").value; // Wert aus #palLevelSelect wird in Variable pal gespeichert
+    if (geschlecht === true) {
+        // Wenn #male checked ist, dann wird der Grundumsatz nach der Harris-Benedict-Formel für Männer berechnet
+        grundumsatz = 66.47 + 13.7 * weight + 5 * height - 6.8 * age;
+    } else {
+        // Wenn #male nicht checked ist, dann wird der Grundumsatz nach der Harris-Benedict-Formel für Frauen berechnet
+        grundumsatz = 655.1 + 9.6 * weight + 1.8 * height - 4.7 * age;
+    }
+    gesamtumsatz = grundumsatz * pal; // Der Gesamtumsatz wird berechnet
+    document.querySelector("#resultValue").textContent = ` ${grundumsatz.toFixed(0)} kcal`; // Der Grundumsatz wird in #resultValue ausgegeben
+    document.querySelector("#resultValuePal").textContent = ` ${gesamtumsatz.toFixed(0)} kcal`; // Der Gesamtumsatz wird in #resultValuePal ausgegeben
 };
 
 // Eventlistener für #calcButton
